2ad13e0a6fbdefc2300f4070d652085bef8460a3
ShadowTux <shadowtux@shadowoftux.tech>
2023-08-16 04:38:50 +0300

    makepify working code

--- nitterify-usernames/bird-makeupify.py
+++ nitterify-usernames/bird-makeupify.py
@@ -2,9 +2,22 @@
 with open("twitter-usernames.txt", "r") as f:
     usernames = f.read().splitlines()
 
-# Transform usernames into the desired format
-formatted_usernames = [f"{username}@bird.makeup" if username.startswith("@") else f"@{username}@bird.makeup" for username in usernames]
+# Filter out @error-co usernames
+filtered_usernames = [username for username in usernames if username.lower() != "@error-co"]
 
-# Print the formatted usernames
+# Transform usernames into the desired format
+formatted_usernames = []
+for username in filtered_usernames:
+    formatted_username = f"{username}@bird.makeup" if username.startswith("@") else f"@{username}@bird.makeup"
+    formatted_usernames.append(formatted_username)
+
+# Prepare the CSV data
+csv_data = "Account address,Show boosts,Notify on new posts,Languages\n"
 for formatted_username in formatted_usernames:
-    print(formatted_username)
+    csv_data += f"{formatted_username},true,false,\n"
+
+# Write the CSV data to the output file
+with open("output.csv", "w") as f:
+    f.write(csv_data)
+
+print("CSV data written to output.csv")
